695d2af6972ac2f99f83eb3b7d5c5197acc9ccb6
AlejandroSantorum <alejandro.santorum@gmail.com>
2018-02-08 23:54:26 +0100

    Rafa manda

--- Tutorial/defines.c
+++ Tutorial/defines.c
@@ -2,17 +2,18 @@
 
 int rand_num(int inf, int sup){
     int res = 0;
-    
+
     if(inf == sup){
         return sup;
     }
-    
+
     else if (inf > sup){
         printf("Error. Defines-F1-1.\n");
         exit(ERROR);
     }
 
     res = (inf + ((int) ((((double)(sup-inf+1)) * rand())/(RAND_MAX + 1.0))));
-    
+
     return res;
+    ;
 }
